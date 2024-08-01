e5cb9cf4419cbb37c1eac1e4be06760a3f880072
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-01 23:15:48 +0300

    ReviewDomainModelAdded

---
+++ Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace ItemCatalogWebsite.Models
+{
+    public class Review
+    {
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public string ReviewrName { get; set; }
+        public string ReviewrEmail { get; set; }
+    }
+}
